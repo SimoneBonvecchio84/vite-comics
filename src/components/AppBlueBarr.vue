4c62a1362a6e63e299051988828b2c736c867637
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-03 16:45:26 +0200

    Feat: add style to icons

--- src/components/AppBlueBarr.vue
+++ src/components/AppBlueBarr.vue
@@ -17,12 +17,7 @@ export default {
                     icon: "buy-comics-shop-locator.png",
                     text: "SUBSCRIPTION"
                     
-                },
-                {
-                    icon: "buy-comics-shop-locator.png",
-                    text: "COMIC SHOP LOCATOR"
-
-                },
+                },              
                 {
                     icon: "buy-dc-power-visa.svg",
                     text: "DC POWER VISA"
@@ -43,8 +38,9 @@ export default {
     <section>
         <div class="general-container">
             <ul>
-                <li>
-                    <img v-for="curIcon in inconsList" :src="getIconsUrl(curIcon.icon)" alt="">
+                <li v-for="curIcon in inconsList" >
+                    <img :src="getIconsUrl(curIcon.icon)" alt="">
+                    <h4> {{ curIcon.text }}</h4>
                 </li>
             </ul>
         </div>
@@ -53,10 +49,32 @@ export default {
 
 <style scoped lang="scss">
 @use "../style/partials/variables" as *;
-
+@use "../style/partials/mixin" as *;
 section {
     background-color: $primary-color;
     padding: 3rem 0;
     color: white;
+
+
+    ul {
+        list-style-type: none;
+        display: flex;
+        justify-content: space-evenly;
+
+        li {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+            img {
+                height: 50px;
+            }
+
+            h4 {
+                font-size: .7rem;
+                font-weight: 300;
+            }
+
+        }
+    }
 }
 </style>
