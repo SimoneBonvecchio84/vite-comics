4039475c31ed64fce2c7cb9d3a99db4583b8c679
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-04 18:01:34 +0200

    Feat: Create AppCardDc and impor to AppMain.vue, Create props with 2 keys(img and title)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
+import AppHeader   from './components/AppHeader.vue';
+import AppMain     from './components/AppMain.vue';
 import AppBlueBarr from './components/AppBlueBarr.vue';
 import AppListSite from './components/AppListSite.vue';
-import AppFooter from './components/AppFooter.vue';
+import AppFooter   from './components/AppFooter.vue';
 export default {
   components: {
     AppHeader,
---
+++ src/components/AppCardDc.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+   props: {
+    cardImage  : String,
+    cardSeries : String,
+   }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="cardImage" :alt="cardSeries">
+        <h4>{{ cardSeries }}</h4>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
